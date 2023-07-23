374286a4e53dc7283dc9df2245cfcf8bd465bd30
JotaRata <jjbarria.c@gmail.com>
2023-07-23 18:21:38 -0400

    Added load_folder method

--- startrak/io.pyx
+++ startrak/io.pyx
@@ -4,6 +4,17 @@ from astropy.io import fits
 from startrak.types import FileInfo
 
 
+# ----- Wrapper functions around astropy.io --------
 def load_file(path: str, *args, **kwargs) -> FileInfo:
     with fits.open(path, *args, **kwargs) as hdu:
         return FileInfo.FromHDU(hdu)
+
+def retrieve_data(fileInfo : FileInfo):
+    return fits.getdata(fileInfo.path)
+
+def load_folder(dir: str, *args, **kwargs):
+    for entry in os.scandir(dir):
+        if not entry.is_file() and not entry.name.endswith(
+            ('.fit', '.fits', '.FIT', '.FITS')):
+            continue
+        yield load_file(entry.path)
