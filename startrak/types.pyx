cbf0a206763f7568654ed5f2b2112a4dc2d45d4d
JotaRata <jjbarria.c@gmail.com>
2023-07-23 20:53:56 -0400

    Added Session types

--- startrak/types.pyx
+++ startrak/types.pyx
@@ -1,13 +1,13 @@
 import os
 from enum import Enum
+from datetime import datetime
+from abc import ABC, abstractmethod, abstractstaticmethod
 from dataclasses import dataclass
 from astropy.io import fits
 
 class SessionType(Enum):
     ASTRO_SCAN = 1
     ASTRO_INSPECT = 2
-
-
 @dataclass(frozen=True)
 cdef class FileInfo():
     cdef readonly str path
@@ -15,7 +15,8 @@ cdef class FileInfo():
     cdef readonly dict[str, str] header
     cdef bint validated
 
-    def FromHDU(hdu: fits.HDUList):
+    @staticmethod
+    def fromHDU(hdu: fits.HDUList):
         if hdu is None: raise TypeError("No HDU list was given")
         if len(hdu) == 0: raise TypeError("Invalid HDU")
         ver = hdu.verify('fix')
@@ -26,3 +27,35 @@ cdef class FileInfo():
         header = {key : _header[key] for key in _header}
 
         return FileInfo(path, sbytes, header, ver)
+
+# ------------- Sessions --------------
+@dataclass
+class SessionBase(ABC):
+    sessionName : str
+    sessionTime : datetime
+    sessionType : SessionType
+
+    @abstractstaticmethod
+    def create(name, *args, **kwargs) -> SessionBase: pass
+    @abstractmethod 
+    def save(self, out): pass
+
+@dataclass
+class InspectionSession(SessionBase):
+    source_files  : list[FileInfo]
+    # Add stars, trackers, settings
+    def create(name : str, source_files : list[FileInfo],*args, **kwargs) -> SessionBase:
+        time =  datetime.utcnow()
+        return InspectionSession(name, time, SessionType.ASTRO_INSPECT
+                , source_files)
+    def save(self, out : str):
+        pass    # todo: Add logic for saving sessions
+@dataclass
+class ScanSession(SessionBase):
+    scandir : str
+    def create(name, scan_dir, *args, **kwargs) -> SessionBase:
+        time =  datetime.utcnow()
+        return ScanSession(name, time, SessionType.ASTRO_SCAN, scan_dir)
+    def save(self, out):
+        pass
+# -------------------------------------
