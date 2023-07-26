b7f5ae09aa8089291d4e891611e5f5375dc94f26
JotaRata <jjbarria.c@gmail.com>
2023-07-26 00:31:16 -0400

    Moved Sessions related funcions to a new file

---
+++ startrak/sessions.pyx
@@ -0,0 +1,28 @@
+from startrak.types import SessionBase, InspectionSession, ScanSession
+from startrak.utils import extension_method
+from enum import StrEnum
+
+
+class SessionType(StrEnum):
+		ASTRO_INSPECT = 'inspect'
+		ASTRO_SCAN = 'scan'
+		
+@extension_method(SessionBase, static=True)
+def Create(sessionType : SessionType, name : str, *args, **kwargs):
+		'''
+			Extesion factory method to create sessions using the SessionType enum
+			Parameters:
+			- sessionType (SessionType): The type of the new session. Accepted values are:
+				- - SessionType.ASTRO_INSPECT = Analyze already saved files in the disk
+				- - SessionType.ASTRO_SCAN = Load and Analyze files as they are being created in a folder
+
+			- name (str): Name of the new session.
+		'''
+		session : SessionBase = None
+		if sessionType == SessionType.ASTRO_INSPECT:
+				session = object.__new__(InspectionSession).__post_init__()
+		elif sessionType == SessionType.ASTRO_SCAN:
+				session = object.__new__(ScanSession).__post_init__()
+		else: raise TypeError('Invalid case')
+
+		return session._create(name, *args, **kwargs)
