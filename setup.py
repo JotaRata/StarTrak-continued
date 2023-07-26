7bc7d8c5517e54199c1f5c09796c737fbf825304
JotaRata <jjbarria.c@gmail.com>
2023-07-26 01:00:35 -0400

    Updated setup.py automation

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 import os
-from os.path import relpath, splitext
+from os.path import relpath, splitext, join
 
-cwd = os.getcwd() + '/startrak'
-extensions = [Extension("startrak." + splitext(path)[0], ['startrak/' + path ]) 
-              for path in os.listdir(cwd) if path.endswith('.pyx')]
+def scan_files():
+	cwd = os.getcwd()
+	for path, _, files in os.walk(cwd + '/startrak'):
+		for f in files:
+			if not f.endswith('.pyx'): continue
+			rel =  relpath(join(path, f), cwd)
+			yield (splitext(rel)[0].replace(os.sep, '.'),  [rel])
+
+extensions = [Extension(*sources) for sources in scan_files()]
 
 setup(
-    name= 'startrak',
-    version="0.1",
-    ext_modules=cythonize(extensions),
-    zip_safe=False,
+		name= 'startrak',
+		version="0.1",
+		ext_modules=cythonize(extensions),
+		zip_safe=False,
 )
