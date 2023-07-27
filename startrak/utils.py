04c7494d508e304eca7a760575d537b8bc0477a3
JotaRata <jjbarria.c@gmail.com>
2023-07-27 02:14:38 -0400

    Added optional paraeter 'name' to extension_method

--- startrak/utils.py
+++ startrak/utils.py
@@ -1,10 +1,10 @@
-def extension_method(target_cls, static = False):
+def extension_method(target_cls, static = False, name = None):
 		def decorator(func):
 				if static:
 					def wrapper(*args, **kwargs): return func( *args, **kwargs)
 				else:
 					def wrapper(self, *args, **kwargs): return func(*args, **kwargs)
 				
-				setattr(target_cls, func.__name__, wrapper)
+				setattr(target_cls, func.__name__ if name is None else name, wrapper)
 				return wrapper
 		return decorator
