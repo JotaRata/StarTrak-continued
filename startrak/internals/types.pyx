dd3fe45f74c610ca2bcb41f76e17878c2c14a896
JotaRata <jjbarria.c@gmail.com>
2023-07-29 01:12:56 -0400

    Added function validate() to Header archetype with an extra callback method

--- startrak/internals/types.pyx
+++ startrak/internals/types.pyx
@@ -1,10 +1,11 @@
+from operator import call
 import os
 from enum import Enum
 from startrak.internals.exceptions import *
 from datetime import datetime
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import Any, cast
+from typing import Any, cast, Callable
 from astropy.io import fits
 
 @dataclass(frozen=True)
@@ -29,17 +30,17 @@ cdef class FileInfo():
         return FileInfo(path, sbytes, header)
 
 cdef class Header():
-    cdef dict items
+    cdef dict _items
     def __init__(self, source : fits.Header | dict):
         allowed_types = (int, bool, float, str)
-        self.items = {str(key) : value for key, value in source.items() 
+        self._items = {str(key) : value for key, value in source.items() 
             if type(value) in allowed_types}
     def __getattr__(self, __name: str) -> Any:
-        return self.items[__name]
+        return self._items[__name]
     def __setattr__(self, __name: str, __value: Any) -> None:
         raise ImmutableError(self)
     def __repr__(self) -> str:
-        return '\n'.join([f'{k} = {v}' for k,v in self.items.items()])
+        return '\n'.join([f'{k} = {v}' for k,v in self._items.items()])
 
 cdef class HeaderArchetype(Header):
     def __init__(self, source : fits.Header | dict) -> None:
@@ -49,10 +50,16 @@ cdef class HeaderArchetype(Header):
         _exptime = float(source['EXPTIME'])
         _naxisn = tuple(int(source[f'NAXIS{n + 1}']) for n in range(self.NAXIS))
         
-        self.items = {'SIMPLE':_simple, 'BITPIX':_bitpix,
+        self._items = {'SIMPLE':_simple, 'BITPIX':_bitpix,
                         'NAXIS':_naxis, 'EXPTIME':_exptime}
-        for n in range(_naxis): self.items[f'NAXIS{n+1}'] = _naxisn[n]
-
+        for n in range(_naxis): self._items[f'NAXIS{n+1}'] = _naxisn[n]
+    
+    def validate(self, header : Header, failed : Callable[[str, Any, Any]] = None):
+        for key, value in self._items.items():
+            if (key not in header._items.keys()) or (header._items[key] != value):
+                if callable(failed): failed(key, value, header._items[key])
+                return False
+        return True
 
 # ------------- Sessions --------------
 class Session(ABC):
