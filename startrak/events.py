24a0cb1cef3f5ebbca54b0581bd34ef1eabecb12
JotaRata <jjbarria.c@gmail.com>
2023-07-30 02:10:08 -0400

    Added more static methods to NamedEvent

--- startrak/events.py
+++ startrak/events.py
@@ -12,18 +12,25 @@ class Event():
 		for function in self._methods: function(*args, **kwagrs)
 	
 class NamedEvent(Event):
-	_named_events = dict()
+	_named_events = dict[str, Event]()
 
 	def __init__(self, name : str, *method_list):
 		super().__init__(*method_list)
 		self.name = str(name)
 		NamedEvent._named_events[self.name, self]
-	@staticmethod
-	def events():
-		return NamedEvent._named_events.values()
 	
 	def forget(self):
 		NamedEvent._named_events.pop(self.name)
 	def __repr__(self) -> str:
 		return self.name +' : '+super().__repr__()
+	
+	@staticmethod
+	def get_events():
+		return NamedEvent._named_events.values()
+	def get_event(name : str):
+		return NamedEvent._named_events[name]
+	@staticmethod
+	def register_to(named_event : str, function : Callable):
+		if named_event in NamedEvent._named_events.keys():
+			NamedEvent._named_events[named_event].add(function)
 	
