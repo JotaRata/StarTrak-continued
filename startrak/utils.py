265987dd2665e81c8e67e118915f8b2872061571
JotaRata <jjbarria.c@gmail.com>
2023-07-30 02:09:51 -0400

    Added decorator to register functions to events

--- startrak/utils.py
+++ startrak/utils.py
@@ -1,3 +1,6 @@
+from startrak.events import NamedEvent
+
+
 def extension_method(target_cls, static = False, name = None):
 		def decorator(func):
 				if static:
@@ -8,3 +11,11 @@ def extension_method(target_cls, static = False, name = None):
 				setattr(target_cls, func.__name__ if name is None else name, wrapper)
 				return wrapper
 		return decorator
+
+def called_by(named_event : str):
+	def decorator(func):
+		def wrapper(*args, **kwargs):
+			return func(*args, **kwargs)
+		NamedEvent.register_to(named_event, wrapper)
+		return wrapper
+	return decorator
