79fd5bba9ad73c6950fcc6bfaca0d1756f05f5a8
JotaRata <jjbarria.c@gmail.com>
2023-07-30 02:01:24 -0400

    Added NamedEvent class

--- startrak/events.py
+++ startrak/events.py
@@ -11,3 +11,19 @@ class Event():
 	def __call__(self, *args, **kwagrs):
 		for function in self._methods: function(*args, **kwagrs)
 	
+class NamedEvent(Event):
+	_named_events = dict()
+
+	def __init__(self, name : str, *method_list):
+		super().__init__(*method_list)
+		self.name = str(name)
+		NamedEvent._named_events[self.name, self]
+	@staticmethod
+	def events():
+		return NamedEvent._named_events.values()
+	
+	def forget(self):
+		NamedEvent._named_events.pop(self.name)
+	def __repr__(self) -> str:
+		return self.name +' : '+super().__repr__()
+	
