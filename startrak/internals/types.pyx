0845735ff044e580339301197582f008999d47be
JotaRata <jjbarria.c@gmail.com>
2023-08-01 17:41:40 -0400

    Updated Types imports + added pxd definitions file

--- startrak/internals/types.pyx
+++ startrak/internals/types.pyx
@@ -3,7 +3,7 @@ cimport numpy as np
 import os
 from enum import Enum
 
-from startrak.internals.exceptions import *
+from startrak.exceptions import *
 from datetime import datetime
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
@@ -12,7 +12,6 @@ from astropy.io import fits
 
 # ---------------------- Headers -------------------------------
 cdef class Header():
-    cdef dict _items
     def __init__(self, source : fits.Header | dict):
         allowed_types = (int, bool, float, str)
         self._items = {str(key) : value for key, value in source.items() 
@@ -51,10 +50,6 @@ cdef class HeaderArchetype(Header):
 # -------------- Files ----------------
 @dataclass(frozen=True)
 cdef class FileInfo():
-    cdef readonly str path
-    cdef readonly int size
-    cdef readonly Header header
-
     @staticmethod
     def fromHDU(hduList: fits.HDUList | Any):
         if hduList is None: raise TypeError("No HDU list was given")
@@ -71,82 +66,29 @@ cdef class FileInfo():
         return FileInfo(path, sbytes, header)
 
 # ------------- Sessions --------------
-class Session(ABC):
-    currentSession : Session
-    
-    #@self.name = 'New Session'
-    #@self.working_dir : str 
-    #@self.archetype : HeaderArchetype 
-    #@self.tracked_items : set[FileInfo]
-    #@self.creation_time = datetime.now()
-    
-    def __init__(self):
-        raise InstantiationError(self, 'Session.Create')
 
-    def __repr__(self) -> str:
-        return f'{type(self).__name__}: ' + str(", ".join(
-            [f'{k} = {v}' for k, v in self.__dict__.items()]))
-    
-    def __post_init__(self):
-        self.name = 'New Session'
-        self.working_dir : str = str()
-        self.archetype : HeaderArchetype = None
-        self.tracked_items : set[FileInfo] = set()
-        self.creation_time = datetime.now()
-        return self
+# class InspectionSession(Session):
+#     def _create(session, name : str, *args, **kwargs) -> Session:
+#         session.name = name
+#         return session
 
-    def add_item(self, item : Any | list): 
-        _items = item if type(item) is list else [item]
-        _added = {_item for _item in _items if type(_item) is FileInfo}
-        if len(self.tracked_items) == 0:
-            first = next(iter(_added))
-            assert isinstance(first, FileInfo)
-            self.set_archetype(first.header)
-        
-        self.tracked_items |= _added
-        self.__item_added__(_added)
-        # todo: raise warning if no items were added
+#     def __item_added__(self, added): pass
+#     def __item_removed__(self, removed): pass
 
-    def remove_item(self, item : Any | list): 
-        _items = item if type(item) is list else [item]
-        _removed = {_item for _item in _items if type(_item) is FileInfo}
-        self.tracked_items -= _removed
-        self.__item_removed__(_removed)
-    
-    def set_archetype(self, header : Header):
-        if header is None: self.archetype = None
-        self.archetype = HeaderArchetype(header)
-    @abstractmethod
-    def _create(self, name, *args, **kwargs) -> Session: pass
-    @abstractmethod
-    def __item_added__(self, added): pass
-    @abstractmethod
-    def __item_removed__(self, removed): pass
-    @abstractmethod 
-    def save(self, out): pass
+#     def save(self, out : str):
+#         pass    # todo: Add logic for saving sessions
 
-class InspectionSession(Session):
-    def _create(session, name : str, *args, **kwargs) -> Session:
-        session.name = name
-        return session
+# class ScanSession(Session):
+#     def _create(session, name, scan_dir, *args, **kwargs) -> Session:
+#         session.name = name
+#         session.working_dir = scan_dir
+#         return session
 
-    def __item_added__(self, added): pass
-    def __item_removed__(self, removed): pass
+#     def __item_added__(self, added): pass
+#     def __item_removed__(self, removed): pass
 
-    def save(self, out : str):
-        pass    # todo: Add logic for saving sessions
-
-class ScanSession(Session):
-    def _create(session, name, scan_dir, *args, **kwargs) -> Session:
-        session.name = name
-        session.working_dir = scan_dir
-        return session
-
-    def __item_added__(self, added): pass
-    def __item_removed__(self, removed): pass
-
-    def save(self, out):
-        pass
+#     def save(self, out):
+#         pass
 
 # ----------------- Data types --------------------
 
