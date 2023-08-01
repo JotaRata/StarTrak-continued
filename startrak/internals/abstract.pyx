451e702f49c16753d5df2bc89fd6a176f86ca6c9
JotaRata <jjbarria.c@gmail.com>
2023-08-01 17:41:00 -0400

    Converted Session class to Cython extension class + Added Cython Interface class and decorator

--- startrak/internals/abstract.pyx
+++ startrak/internals/abstract.pyx
@@ -1,60 +1,72 @@
 from startrak.exceptions import *
-from startrak.internals.types import FileInfo, HeaderArchetype, Header
-from abc import ABC, abstractmethod
-from typing import Any
+from startrak.internals.types cimport FileInfo, HeaderArchetype, Header
+from abc import ABC, abstractmethod, ABCMeta
+from typing import Any, Protocol
 import datetime
 
+ctypedef fused _FileOrList:
+	FileInfo
+	list
 
-class Session(ABC):
-    currentSession : Session 	# todo: move somewhere else
-    
-    #@self.name = 'New Session'
-    #@self.working_dir : str 
-    #@self.archetype : HeaderArchetype 
-    #@self.tracked_items : set[FileInfo]
-    #@self.creation_time = datetime.now()
-    
-    def __init__(self):
-        raise InstantiationError(self, 'Session.Create')
+cdef class Interface:
+	def __cinit__(self):
+		_type = type(self)
+		if _type.__base__ is Interface or _type is Interface:
+			raise InstantiationError(self)
+		
+def abstract(func):
+	def wrapper(*args, **kwargs):
+		raise NotImplementedError(func.__name__)
+	return wrapper
 
-    def __repr__(self) -> str:
-        return f'{type(self).__name__}: ' + str(", ".join(
-            [f'{k} = {v}' for k, v in self.__dict__.items()]))
-    
-    def __post_init__(self):
-        self.name = 'New Session'
-        self.working_dir : str = str()
-        self.archetype : HeaderArchetype = None
-        self.tracked_items : set[FileInfo] = set()
-        self.creation_time = datetime.now()
-        return self
+cdef class Session(Interface):
+		currentSession : Session 	# todo: move somewhere else
+		cdef str name
+		cdef str working_dir
+		cdef HeaderArchetype archetype
+		cdef set[FileInfo] included_files
 
-    def add_item(self, item : Any | list): 
-        _items = item if type(item) is list else [item]
-        _added = {_item for _item in _items if type(_item) is FileInfo}
-        if len(self.tracked_items) == 0:
-            first = next(iter(_added))
-            assert isinstance(first, FileInfo)
-            self.set_archetype(first.header)
-        
-        self.tracked_items |= _added
-        self.__item_added__(_added)
-        # todo: raise warning if no items were added
+		def __repr__(self) -> str:
+				return f'{type(self).__name__}: ' + str(", ".join(
+						[f'{k} = {v}' for k, v in self.__dict__.items()]))
+		
+		def __post_init__(self):
+				self.name = 'New Session'
+				self.working_dir = str()
+				self.archetype = None
+				self.included_files  = set()
+				# self.creation_time = datetime.now()
+				return self
 
-    def remove_item(self, item : Any | list): 
-        _items = item if type(item) is list else [item]
-        _removed = {_item for _item in _items if type(_item) is FileInfo}
-        self.tracked_items -= _removed
-        self.__item_removed__(_removed)
-    
-    def set_archetype(self, header : Header):
-        if header is None: self.archetype = None
-        self.archetype = HeaderArchetype(header)
-    @abstractmethod
-    def _create(self, name, *args, **kwargs) -> Session: pass
-    @abstractmethod
-    def __item_added__(self, added): pass
-    @abstractmethod
-    def __item_removed__(self, removed): pass
-    @abstractmethod 
-    def save(self, out): pass
+		def add_item(self, _FileOrList item): 
+				_items = item if type(item) is list else [item]
+				_added = {_item for _item in _items if type(_item) is FileInfo}
+				if len(self.included_files) == 0:
+						first = next(iter(_added))
+						assert isinstance(first, FileInfo)
+						self.set_archetype(first.header)
+				
+				self.included_files |= _added
+				self.__item_added__(_added)
+				# todo: raise warning if no items were added
+
+		def remove_item(self, _FileOrList item): 
+				_items = item if type(item) is list else [item]
+				_removed = {_item for _item in _items if type(_item) is FileInfo}
+				self.included_files -= _removed
+				self.__item_removed__(_removed)
+		
+		def set_archetype(self, Header header):
+				if header is None: self.archetype = None
+				self.archetype = HeaderArchetype(header)
+		@abstract
+		def _create(self, str name, *args, **kwargs) -> Session: pass
+		@abstract
+		def __item_added__(self, added): pass
+		@abstract
+		def __item_removed__(self, removed): pass
+		@abstract 
+		def save(self, out): pass
+
+class SubSession(Session):
+	pass
