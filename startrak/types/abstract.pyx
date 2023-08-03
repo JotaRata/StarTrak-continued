2879335e508934a0940eec5be0414764ea72ce20
JotaRata <jjbarria.c@gmail.com>
2023-08-02 22:03:44 -0400

    Refactored Interface class

--- startrak/types/abstract.pyx
+++ startrak/types/abstract.pyx
@@ -1,4 +1,3 @@
-from re import A
 from startrak.internals.types import InstantiationError
 
 
@@ -10,23 +9,27 @@ cdef class Interface:
 		if cls is Interface or cls.__base__ is Interface:
 			raise TypeError(f'{cls.__name__} is marked as an Interface and cannot be instantiated')
 		assert isinstance(self, Interface)
-		cls.__base__.__initsubclass__()
-		cls.__base__.__subclasshook__(cls)
-
+		cls.__cls_init__()
+		cls.__base__.__initsubclass__(cls)
+	
 	@classmethod
-	def __initsubclass__(cls, **kwargs):
-		if getattr(cls, "__initialized__", False):
-			return NotImplemented
-		methods = getattr(cls, "__methods__", set())
+	def __cls_init__(cls):
+		if cls is Interface: return
+		if not getattr(cls.__base__, "__initialized__", False):
+			cls.__base__.__cls_init__()
+		methods = set(getattr(cls, "__methods__", set()))
 		for name, value in cls.__dict__.items():
 			if callable(value) and getattr(value, "__isabstractmethod__", False):
-					methods.add(name)
+				methods.add(name)
+			if (_base := cls.__base__) is not Interface:
+				if callable(value) and name in _base.__methods__:
+					methods.remove(name)
+
 		cls.__methods__ = frozenset(methods)
 		cls.__initialized__ = True
+	
 	@classmethod
-	def __subclasshook__(cls, subclass):
-		if cls.__base__ is not Interface:
-				return NotImplemented
+	def __initsubclass__(cls, subclass):
 		for method in cls.__methods__:
 				if not callable(subclass.__dict__.get(method, None)):
 						raise TypeError(f'{subclass.__name__} does not implement abstract method: {method}')
@@ -36,7 +39,3 @@ def abstract(func):
 		return func(*args, **kwargs)
 	setattr(wrapper, '__isabstractmethod__', True)
 	return wrapper
-
-class Test(Interface):
-	@abstract
-	def foo(self, x): pass
