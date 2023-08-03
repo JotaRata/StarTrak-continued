3d685600ae0c739ca8c5ad42187f71ee74030557
JotaRata <jjbarria.c@gmail.com>
2023-08-03 19:15:01 -0400

    Included Header classes in types.__init__

--- startrak/types/__init__.pxd
+++ startrak/types/__init__.pxd
@@ -2,7 +2,15 @@
 # file: "C:\Users\jjbar\Documents\GitHub\StarTrak-continued/startrak\types\__init__.pyx"
 
 import os.path
-from startrak.internals.types cimport Header
 from astropy.io import fits as _astropy
-cdef class FileInfo(): pass
-cdef class Star(): pass
+
+cdef class Header():
+	cdef dict _items
+cdef class HeaderArchetype(Header): pass
+cdef class FileInfo():
+	cdef readonly str path
+	cdef readonly int size
+	cdef readonly Header header
+cdef class Star():
+	cdef public str name
+	cdef public int[2] position
--- startrak/types/__init__.pyx
+++ startrak/types/__init__.pyx
@@ -1,30 +1,55 @@
 import os.path
-from startrak.internals.types cimport Header
 from astropy.io import fits as _astropy
 
-cdef class FileInfo():
-	cdef readonly str path
-	cdef readonly int size
-	cdef readonly Header header
 
+cdef class Header():
+	def __init__(self, source : _astropy.Header | dict):
+		allowed_types = (int, bool, float, str)
+		self._items = {str(key) : value for key, value in source.items() 
+			if type(value) in allowed_types}
+	
+	def contains_key(self, key : str):
+		return key in self._items.keys()
+	def __getitem__(self, key : str):
+		return self._items[key]
+	def __getattr__(self, __name: str):
+		return self._items[__name]
+	def __repr__(self) -> str:
+		return '\n'.join([f'{k} = {v}' for k,v in self._items.items()])
+
+cdef class HeaderArchetype(Header):
+	def __init__(self, source : Header | dict):
+		_simple = source['SIMPLE'] == 1
+		_bitpix = int(source['BITPIX'])
+		_naxis = int(source['NAXIS'])
+		_exptime = float(source['EXPTIME'])
+		_naxisn = tuple(int(source[f'NAXIS{n + 1}']) for n in range(_naxis))
+		
+		self._items = {'SIMPLE':_simple, 'BITPIX':_bitpix,
+							'NAXIS':_naxis, 'EXPTIME':_exptime}
+		for n in range(_naxis): self._items[f'NAXIS{n+1}'] = _naxisn[n]
+	
+	def validate(self, Header header, failed : callable):
+		for key, value in self._items.items():
+			if (key not in header._items.keys()) or (header._items[key] != value):
+					if callable(failed): failed(key, value, header._items[key])
+					return False
+		return True
+
+cdef class FileInfo():
 	def __init__(self, hduList : _astropy.HDUList):
 		if hduList is None: raise TypeError("No HDU list was given")
 		if len(hduList) == 0: raise TypeError("Invalid HDU")
-		path = hduList.filename()
+		self.path = hduList.filename()
 		hdu = hduList[0]
-		assert isinstance(path, str)
+		assert isinstance(self.path, str)
 		assert isinstance(hdu, _astropy.PrimaryHDU)
 
-		sbytes = os.path.getsize(path)
+		self.size = os.path.getsize(self.path)
 		_header = hdu.header
-		
-		header = Header(_header)
-		return FileInfo(path, sbytes, header)
+		self.header = Header(_header)
 
 cdef class Star():
-	cdef public str name
-	cdef public int[2] position
-
 	def __init__(self, str name, tuple position):
 		self.name = name
 		assert len(position) == 2
