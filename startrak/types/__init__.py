6b1010314e4ff5249b4a56f286f952049858a8d6
JotaRata <jjbarria.c@gmail.com>
2023-08-08 19:55:02 -0400

    Minor myppyc syntax work

--- startrak/types/__init__.py
+++ startrak/types/__init__.py
@@ -1,18 +1,19 @@
 from abc import ABC, abstractmethod
+from dataclasses import FrozenInstanceError, dataclass
 import os.path
-from typing import Callable, Dict, Optional, Self, Tuple
+from typing import Any, Callable, Dict, Optional, Self, Tuple, overload
 from astropy.io import fits as _astropy # type: ignore
 from numpy import ndarray
 
-__header_allowed_types : Tuple[type, type, type, type]  = (int, bool, float, str)
 __archetype_entries : Dict[str, type] = {'SIMPLE' : int, 'BITPIX' : int,
 											'NAXIS' : int, 'EXPTIME' : float}
 __archetype_user_entries : Dict[str, type] = {}
 
 class Header():
+	_items : Dict[str, int | bool | float | str]
 	def __init__(self, source : _astropy.Header | dict):
 		self._items = {str(key) : value for key, value in source.items() 
-			if type(value) in __header_allowed_types}
+			if type(value) in (int, bool, float, str)}
 	
 	def contains_key(self, key : str):
 		return key in self._items.keys()
@@ -34,6 +35,7 @@ class HeaderArchetype(Header):
 		
 		assert 'NAXIS' in self._items
 		_naxis = self._items['NAXIS']
+		assert type(_naxis) is int
 		_naxisn = tuple(int(source[f'NAXIS{n + 1}']) for n in range(_naxis))
 		for n in range(_naxis): self._items[f'NAXIS{n+1}'] = _naxisn[n]
 	
@@ -47,56 +49,58 @@ class HeaderArchetype(Header):
 	@staticmethod
 	def set_keywords(user_keys : Dict[str, type]):
 		assert all([ type(key) is str  for key in user_keys])
-		assert all([ value in __header_allowed_types for value in user_keys.values()])
+		assert all([ value in (int, bool, float, str) for value in user_keys.values()])
 		global __archetype_user_entries
 		__archetype_user_entries = user_keys
 
 class FileInfo():
-	def __init__(self, *args):
-		if len(args) == 1 and type(path := args[0]) is str:
+	path : str
+	size : int
+	header : Header
+	def __init__(self, source : str | _astropy.HDUList):
+		if type(path := source) is str:
 			with _astropy.open(path) as hdu:
 				self.path = os.path.abspath(path)
 				self.size = os.path.getsize(path)
 				phdu = hdu[0]
 				assert isinstance(phdu, _astropy.PrimaryHDU)
 				self.header = Header(phdu.header)
-				return
-		elif len(args) == 1 and type(hdu := args[0]) is _astropy.HDUList:
+		elif type(hdu := source) is _astropy.HDUList:
 				self.path = os.path.abspath(hdu.filename())
 				self.size = os.path.getsize(self.path)
 				phdu = hdu[0]
 				assert isinstance(phdu, _astropy.PrimaryHDU)
 				self.header = Header(phdu.header)
-				return
 		else:
 			raise TypeError('Expected one argument of type str or HDUList')
+	def __setattr__(self, __name: str, __value: Any) -> None:
+			raise FrozenInstanceError(type(self).__name__)
 	
 	def get_data(self) -> ndarray:
 		return _astropy.getdata(self.path)
 	def __repr__(self):
 		return f'\n[File: "{os.path.basename(self.path)}" ({self.size}) bytes]'
 
+@dataclass
 class Star():
-	def __init__(self, name : str, position : Tuple[int, int], aperture : int):
-		self.name = name
-		assert len(position) == 2
-		self.position = position
-		self.aperture = aperture
+	name : str
+	position : Tuple[int, int]
+	aperture : int
+	
 	@classmethod
 	def From(cls, other : Self, **kwargs) -> Self:
 		return cls(other.name, other.position, other.aperture)
-	def export(self) -> Tuple[str, str, Tuple, int]:
+	def export(self) -> Tuple[str, str, Tuple[int, int], int]:
 		return type(self).__name__, self.name, self.position, self.aperture
 	def __repr__(self):
 		return f'{type(self).__name__}: {self.name}'
 
 class ReferenceStar(Star):
-	def __init__(self, name : str, position : Tuple[int, int], aperture : int, magnitude : float):
-		super().__init__(name, position, aperture)
+	magnitude : float
+	def __init__(self, source : Star, magnitude : float):
+		super().__init__(source.name, source.position, source.aperture)
 		self.magnitude = magnitude
-	@classmethod
-	def From(cls, other : Star, **kwargs) -> Self:
-		return cls(other.name, other.position, other.aperture, kwargs.get('magnitude', 0.0))
+	
 	def export(self):
 		return (*super().export(), self.magnitude)
 
