574e15e0667fe8b54e793d599a59204e3dcb71af
JotaRata <jjbarria.c@gmail.com>
2023-08-15 22:07:38 -0400

    Added a wrapper of a BufferedReader class to handle FITS reading

--- startrak/types/fits.py
+++ startrak/types/fits.py
@@ -1,18 +1,37 @@
+from dataclasses import dataclass
 from io import BufferedReader
-from typing import Iterator, Union, Tuple
-
+from typing import BinaryIO, ClassVar, Dict, Final, Generator, Iterator, Type, Union, Tuple, cast
+import numpy as np
+from numpy.typing import NDArray
 _ValueType = Union[int, float, str, bool]
-def _parse_header(_file : BufferedReader) -> Iterator[Tuple[str, _ValueType]]:
-	while True:
-		line = _file.read(80)
-		if not line: break
-		if b'END' in line: break
-		_validate_line(line)
-		_keyword = line[:8].decode().rstrip()
-		_value = _parse_value(line)
-		yield _keyword, _value
-      
-def _parse_value(src : bytes) -> _ValueType:
+import os.path
+
+class _FITSBufferedReaderWrapper:
+	_file : BinaryIO
+	_end_offset : int
+
+	def __init__(self, file_path : str | bytes) -> None:
+		self._file = open(file_path, 'rb')
+		self._end_offset = 0
+
+	def _read_header(self) -> Iterator[Tuple[str, _ValueType]]:
+		while True:
+			line = self._file.read(80)
+			if not line: break
+			if b'END' in line:
+				self._end_offset = self._file.tell()
+				break
+			_validate_byteline(line)
+			_keyword = line[:8].decode().rstrip()
+			_value = _parse_bytevalue(line)
+			yield _keyword, _value
+	
+	def tell(self):
+		return self._file.tell()
+	def close(self):
+		return self._file.close()
+			
+def _parse_bytevalue(src : bytes) -> _ValueType:
 	if src[10] == 39:
 		end = src.rfind(39, 19)
 		return src[11:end].decode()
@@ -24,16 +43,6 @@ def _parse_value(src : bytes) -> _ValueType:
 		return int(num)
 	return num
 
-
-def _validate_line(line : bytes):
+def _validate_byteline(line : bytes):
 	if not (line[8] == 61 and line[9] == 32):
 		raise IOError('Invalid header syntax')
-   
-f = open('tests/sample_files/aefor4.fit', 'rb')
-g = _parse_header(f)
-
-next(g)
-
-ord('T')
-ord('B')
-ord("'")
