0d509751f55400a2499d89c5788964ff6c75953f
JotaRata <jjbarria.c@gmail.com>
2023-08-18 22:05:12 -0400

    Added linear stretch method

--- startrak/imageutils.py
+++ startrak/imageutils.py
@@ -3,9 +3,10 @@ import numpy as np
 
 _ImageLike = np.ndarray[Any, np.dtype[np.int_]]	# todo: Unify typing aliases
 __all__ = ['sigma_stretch']
+
 def sigma_stretch(image : _ImageLike, sigma=1.0):
 	'''
-		Sigma clipping stretch algorithm
+		Sigma clipping linear stretch algorithm
 
 		Parameters:
 		- image (arraylike) : The image to stretch
@@ -15,5 +16,16 @@ def sigma_stretch(image : _ImageLike, sigma=1.0):
 	std = np.std(image)
 	smin = median - sigma * std
 	smax = median + sigma * std
+	image = np.clip((image - smin) * (255 / (smax - smin)), 0, 255)
+	return image.astype(np.uint8)
+
+def linear_stretch(image : _ImageLike, smin : float|int, smax: float|int):
+	'''
+		Linear clipping stretch algorithm
+
+		Parameters:
+		- image (arraylike) : The image to stretch
+		- smin/smax (scalar): Minimum and maximum values to stretch the input image into the 0..255 range
+	'''
 	image = np.clip((image - smin) * (255 / (smax - smin)), 0, 255)
 	return image.astype(np.uint8)
