b1da6adfe42a022138e39821c1a2965f89f3d5a4
JotaRata <jjbarria.c@gmail.com>
2023-08-19 20:09:05 -0400

    Added SimpleTracker class

--- startrak/types/trackers.py
+++ startrak/types/trackers.py
@@ -1,4 +1,21 @@
-from startrak.types import TrackingMethod
+from typing import Any, List, Optional, Tuple
+from startrak.types import Star, TrackingMethod, TrackingModel
+from startrak.types.alias import Position
 
-class SimpleTrack(TrackingMethod):
-	pass
+class SimpleTracker(TrackingMethod):
+	track_size : int
+	var_thresold : float
+	_model : TrackingModel
+	_star_values : List[float]
+
+	def __init__(self, tracking_size : int, variation : float) -> None:
+		self.track_size = tracking_size
+		self.var_thresold = variation
+		self._model = TrackingModel()
+		self._star_values = list[float]()
+	
+	def setup_model(self, stars: List[Star], *args: Tuple):
+		assert len(stars) >= 3, 'There should be at least three trackable stars'
+		self._model.vertices = [star.position for star in stars]
+		# todo: setup star brighness values
+		
