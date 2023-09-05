22ba008bb0432f76c6526e251e4a84dc839c5288
JotaRata <jjbarria.c@gmail.com>
2023-09-05 03:21:51 -0300

    Fixed logic to account for brightness + technical fixxes

--- startrak/types/trackers.py
+++ startrak/types/trackers.py
@@ -1,4 +1,4 @@
-from typing import Any, Iterator, List, Literal, Optional, Tuple, cast
+from typing import Any, Generator, Iterator, List, Literal, Optional, Tuple, cast
 import numpy as np
 from numpy.typing import NDArray
 from startrak.types import Star, Tracker, TrackingModel
@@ -16,38 +16,43 @@ class SimpleTracker(Tracker[PositionArray]):
 		self._factor = variation
 		self._values = list[float]()
 	
-	def setup_model(self, stars: List[Star], *args: Tuple):
+	def setup_model(self, stars: List[Star], *args):
 		assert len(stars) >= 3, 'There should be at least three trackable stars'
-		self._model = np.vstack([star.position for star in stars])
+		self._model = np.vstack([star.position[::-1] for star in stars])
 		# todo: setup star brighness values
 
-	def track(self, images: Iterator[ImageLike]) -> TrackingModel:
+	def track(self, _image : ImageLike):
 		assert self._model is not None, "Tracking model hasn't been set"
 		if self._previous is None:
 			self._previous = self._model.copy()
 		
-		_image = next(images)
+		if self._values is None or len(self._values) == 0:
+			self._values = _image[*self._previous.T].tolist()
 		_positions = []
-		for i, (col, row)  in enumerate(self._previous):
+		for i, (row, col) in enumerate(self._previous):
 			crop = _image[row - self._size : row + self._size,
 								col - self._size : col + self._size]
 			mask = np.abs(self._values[i] - crop) < (self._values[i] * self._factor)
-			indices = np.nonzero(mask)
+			indices = np.transpose(np.nonzero(mask))
+			if len(indices) == 0:
+				_positions.append(self._previous[i])
+				continue
+			
 			_median = np.median(indices, axis= 0)
-			_positions.append(_median)
+			_positions.append(_median - (self._size,) * 2 + self._previous[i])
 
 		self._current = np.vstack(_positions)
 
-		dx = np.mean(self._current[:, 0] - self._previous[:, 0])
-		dy = np.mean(self._current[:, 1] - self._previous[:, 1])
+		dx = np.nanmean(self._current[:, 0] - self._previous[:, 0])
+		dy = np.nanmean(self._current[:, 1] - self._previous[:, 1])
 
-		center : PositionArray = np.mean(self._previous, axis=0)
+		center : PositionArray = np.nanmean(self._previous, axis=0)
 		c_previous = self._previous - center
 		c_current = self._current - center
 
-		_dot = np.sum(c_previous * c_current, axis= 1)
+		_dot = np.nansum(c_previous * c_current, axis= 1)
 		_cross = np.cross(c_previous, c_current)
-		_angle = np.mean(np.arctan2(_cross,  _dot))
+		_angle = np.nanmean(np.arctan2(_cross,  _dot))
 
-		self._previous = self._current
+		self._previous = self._current.astype(int)
 		return TrackingModel(cast(float, dx), cast(float, dy), _angle)
