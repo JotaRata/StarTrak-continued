5ab33228b01745011ae062899ab73616343e0881
JotaRata <jjbarria.c@gmail.com>
2023-11-15 01:56:14 -0300

    Added global alignmnt tracker

--- startrak/types/trackers.py
+++ startrak/types/trackers.py
@@ -1,8 +1,11 @@
+import itertools
+import math
 from typing import List, Tuple, cast
 import cv2
 import numpy as np
-from startrak.native import Star, Tracker, TrackingSolution
+from startrak.native import Star, StarDetector, Tracker, TrackingSolution
 from startrak.native.alias import ImageLike
+from startrak.types.detection import HoughCircles
 from startrak.types.phot import _get_cropped
 
 class SimpleTracker(Tracker):
@@ -78,4 +81,45 @@ class SimpleTracker(Tracker):
 										error= float(error), 
 										origin= cast(TPos, _center), 
 										lost_indices= lost)
+
+class GlobalAlignment(Tracker):
+	_detector : StarDetector
+
+	def __init__(self, **detector_args) -> None:
+		self._detector = HoughCircles(**detector_args)
+
+	def setup_model(self, stars: List[Star]):
+		coords = [star.position for star in stars]
+		self._model = itertools.combinations(coords, 3)
+
+
+	def _compare_triangles(self, trig1, trig2) -> bool:	#type:ignore
+		def distance(point1, point2):
+			return math.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
+		def angle(side1, side2, side3):
+			return math.degrees(math.acos((side1**2 + side2**2 - side3**2) / (2 * side1 * side2)))
+		
+		sides1 = [distance(trig1[i], trig1[(i+1)%3]) for i in range(3)]
+		angles1 = [angle(sides1[i], sides1[(i+1)%3], sides1[(i+2)%3]) for i in range(3)]
+
+		sides2 = [distance(trig2[i], trig2[(i+1)%3]) for i in range(3)]
+		angles2 = [angle(sides2[i], sides2[(i+1)%3], sides2[(i+2)%3]) for i in range(3)]
+
+		# Check if the ratios of corresponding sides and angles are equal
+		side_ratios = [sides1[i] / sides2[i] for i in range(3)]
+		angle_diffs = [abs(angles1[i] - angles2[i]) for i in range(3)]
+		return all(0.95 <= ratio <= 1.05 for ratio in side_ratios) and all(diff < 1 for diff in angle_diffs)
 	
+	def track(self, image: ImageLike) -> TrackingSolution:
+		detected_stars = self._detector.detect(image)
+		coords = [star.position for star in detected_stars]
+		triangles = itertools.combinations(coords, 3)
+
+		# !warning: slow code
+		matched = list[int]()
+		for index, trig1 in enumerate(self._model):
+			for trig2 in triangles:
+				if self._compare_triangles(trig1, trig2):
+					matched.append(index)
+		
+		return TrackingSolution.identity()
