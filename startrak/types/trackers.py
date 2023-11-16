4ed561aadfcdec5ad7285c20b1d34b912af24468
JotaRata <jjbarria.c@gmail.com>
2023-11-16 03:32:15 -0300

    Implemented an article written by Peng Qian(2023) to improve tracking speed

--- startrak/types/trackers.py
+++ startrak/types/trackers.py
@@ -1,3 +1,4 @@
+from calendar import c
 import itertools
 import math
 from typing import List, Tuple, cast
@@ -88,38 +89,81 @@ class GlobalAlignment(Tracker):
 	def __init__(self, **detector_args) -> None:
 		self._detector = HoughCircles(**detector_args)
 
+	def _neighbors(self, pos_array):
+		n = len(pos_array); k = 2
+		
+		a = pos_array.reshape(n, 1)
+		b = pos_array.reshape(1, n)
+		dist = (a['x'] - b['x'])**2 + (a['y'] - b['y'])**2
+		return np.argpartition(dist, k+1, axis=1)[:, :k+1]
+
 	def setup_model(self, stars: List[Star]):
-		coords = [star.position for star in stars]
-		self._model = itertools.combinations(coords, 3)
-
-
-	def _compare_triangles(self, trig1, trig2) -> bool:	#type:ignore
-		def distance(point1, point2):
-			return math.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
-		def angle(side1, side2, side3):
-			return math.degrees(math.acos((side1**2 + side2**2 - side3**2) / (2 * side1 * side2)))
+		# todo: include this in Position class
+		dt = np.dtype([('x', 'int'), ('y', 'int')])
+		coords = np.array([star.position[::-1] for star in stars], dtype= dt)
 		
-		sides1 = [distance(trig1[i], trig1[(i+1)%3]) for i in range(3)]
-		angles1 = [angle(sides1[i], sides1[(i+1)%3], sides1[(i+2)%3]) for i in range(3)]
+		self._indices = self._neighbors(coords)
+		self._model = coords[self._indices]
 
-		sides2 = [distance(trig2[i], trig2[(i+1)%3]) for i in range(3)]
-		angles2 = [angle(sides2[i], sides2[(i+1)%3], sides2[(i+2)%3]) for i in range(3)]
+	def _compare(self, trig1, trig2) -> bool:
+		a1 =  (trig1[0][0] - trig1[1][0])**2 + (trig1[0][1] - trig1[1][1])**2
+		b1 =  (trig1[0][0] - trig1[2][0])**2 + (trig1[0][1] - trig1[2][1])**2
+		if a1 == 0 or b1 == 0: return False
+		c1 =  (trig1[1][0] - trig1[2][0])**2 + (trig1[1][1] - trig1[2][1])**2
+		ang1 = np.arccos((a1 + b1 - c1) / (2 * a1 * b1))
 
-		# Check if the ratios of corresponding sides and angles are equal
-		side_ratios = [sides1[i] / sides2[i] for i in range(3)]
-		angle_diffs = [abs(angles1[i] - angles2[i]) for i in range(3)]
-		return all(0.95 <= ratio <= 1.05 for ratio in side_ratios) and all(diff < 1 for diff in angle_diffs)
-	
+		a2 =  (trig2[0][0] - trig2[1][0])**2 + (trig2[0][1] - trig2[1][1])**2
+		b2 =  (trig2[0][0] - trig2[2][0])**2 + (trig2[0][1] - trig2[2][1])**2
+		if a2 == 0 or b2 == 0: return False
+		c2 =  (trig2[1][0] - trig2[2][0])**2 + (trig2[1][1] - trig2[2][1])**2
+		ang2 = np.arccos((a2 + b2 - c2) / (2 * a2 * b2))
+		return ((0.95 <= a1/a2 < 1.05) and (0.95 <= b1/b2 < 1.05)) and (np.abs(ang2 - ang1) < 0.05)
+
 	def track(self, image: ImageLike) -> TrackingSolution:
+		''' 
+			Based on "Efficient k-Nearest Neighbors (k-NN) Solutions with NumPy" by Peng Qian (2023)
+			https://www.dataleadsfuture.com/efficient-k-nearest-neighbors-k-nn-solutions-with-numpy/
+		'''
 		detected_stars = self._detector.detect(image)
-		coords = [star.position for star in detected_stars]
-		triangles = itertools.combinations(coords, 3)
-
+		dt = np.dtype([('x', 'int'), ('y', 'int')])
+		coords = np.array([star.position[::-1] for star in detected_stars], dtype= dt)
+		triangles = self._neighbors(coords)
+		TPos = Tuple[float, float]
 		# !warning: slow code
-		matched = list[int]()
-		for index, trig1 in enumerate(self._model):
-			for trig2 in triangles:
-				if self._compare_triangles(trig1, trig2):
-					matched.append(index)
+		matched = list[Tuple[int, int]]()
+		for i, trig1 in enumerate(self._model):
+			for j, indices in enumerate(triangles):
+				if self._compare(trig1, coords[indices]):
+					matched.append((i, j))
+					break
+		print(f'Matched {len(matched)} of {len(triangles)} triangles by SAS')
+		if len(matched) <= 1:
+			return TrackingSolution.identity()
+		delta_pos = []
+		delta_rot = []
+		_centroids = []
+		for model_idx, current_idx in matched:
+			model = self._model.view((int, len(self._model.dtype.names)))[model_idx]
+			triangle = coords.view((int, len(coords.dtype.names)))[triangles[current_idx]]
+
+			centroid1 = np.mean(model, axis= 0)
+			centroid2 = np.mean(triangle, axis= 0)
+
+			_dot = np.nansum((model - centroid1) * (triangle - centroid2), axis= 1)
+			_cross = np.cross((model - centroid1), (triangle - centroid2))
+			da = np.nanmean(np.arctan2(_cross,  _dot))
+
+			delta_pos.append(centroid2 - centroid1)
+			delta_rot.append(da)
+			_centroids.append(centroid2)
 		
-		return TrackingSolution.identity()
+		ex, ey = np.nanstd(delta_pos, axis= 0)
+		error = np.sqrt(ex**2 + ey**2)
+		
+		dpos = tuple(np.nanmean(delta_pos, axis= 0).tolist())
+		dangle = np.nanmean(delta_rot)
+		center = tuple(np.nanmean(_centroids, axis= 0).tolist())
+		return TrackingSolution(delta_pos= cast(TPos, dpos),
+										delta_angle= cast(float, dangle),
+										error= error,
+										origin= cast(TPos, center))
