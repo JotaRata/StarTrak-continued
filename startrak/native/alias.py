66ac258af1c176346d0c5166da5734abda7eb640
JotaRata <jjbarria.c@gmail.com>
2023-11-29 02:30:22 -0300

    Removed Position and PositionArray type aliases from aliases

--- startrak/native/alias.py
+++ startrak/native/alias.py
@@ -9,9 +9,6 @@ ValueType = Union[int, float, str, bool]
 NumberLike = Union[np.uint, np.float_]
 ImageLike = NDArray[NumberLike]
 
-Position = Tuple[int, int]
-PositionArray = np.ndarray[ Tuple[int, Literal[2]], np.dtype[np.int_]]
-
 StarList = List[Star]
 
 _DecoratorReturn = TypeVar('_DecoratorReturn')
