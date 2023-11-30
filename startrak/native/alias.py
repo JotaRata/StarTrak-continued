7bd263749870f1669d3ea045fc4e705c80d7e60d
JotaRata <jjbarria.c@gmail.com>
2023-11-29 21:17:56 -0300

    Removed circular import on aliases

--- startrak/native/alias.py
+++ startrak/native/alias.py
@@ -1,18 +1,13 @@
 # This file should only contain type aliases or generic type declarations.
 from __future__ import annotations
 
-from typing import Callable, Collection, List, Literal, Tuple, TypeVar, Union
+from typing import Callable, List, TypeVar, Union
 import numpy as np
 from numpy.typing import NDArray
-from startrak.native import Star
-from startrak.native.collections import Position
 
 ValueType = Union[int, float, str, bool]
 NumberLike = Union[np.uint, np.float_]
 ImageLike = NDArray[NumberLike]
-
-PositionLike = Union[Tuple[int, int], List[int], NDArray[np.int_]]
-StarList = List[Star]
 
 _DecoratorReturn = TypeVar('_DecoratorReturn')
 Decorator = Callable[..., Callable[..., _DecoratorReturn]]
