e459ebea97387eef8486c55809605876cd6af4e8
JotaRata <jjbarria.c@gmail.com>
2024-01-02 19:23:38 -0300

    Changed setuo.py messages

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ from mypyc.build import mypycify # type: ignore
 import os	
 from os.path import relpath, splitext, join
 
+print("Compiling Startrak..\n")
+
 def scan_files():
 	cwd = os.getcwd()
 	for path, _, files in os.walk(cwd + '/startrak/native'):
@@ -13,11 +15,15 @@ def scan_files():
 			if not f.endswith('.py'): continue
 			rel =  relpath(join(path, f), cwd)
 			result = (splitext(rel)[0].replace(os.sep, '.'),  rel)
-			print(result)
 			yield result
 
+print("Files")
 paths = [path for _, path in scan_files()]
+print('\n'.join(paths))
+print("="*40)
+print()
 
+print("Compilation log:")
 setup(
 		name= 'startrak',
 		# packages= ['startrak', 'startrak.native'],
