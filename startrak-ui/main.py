0b9a36dd2d2f748f2564a87fea83f5df671d633c
JotaRata <jjbarria.c@gmail.com>
2024-01-14 01:41:17 -0300

    renamed and structured files

--- startrak-ui/main.py
+++ startrak-ui/main.py
@@ -1,14 +1,14 @@
-import os, sys
-sys.path.insert(0, os.getcwd())
+from pathlib import Path
 from PySide6.QtWidgets import QApplication
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtCore import QFile, QIODevice
 
-		
+file_path = Path(__file__).parent
 if __name__ == "__main__":
 	app = QApplication([])
-	ui_file = QFile("startrak-ui/qt/template/main.ui")
-	ui_file.open(QIODevice.ReadOnly)
+	app.setStyle('Fusion')
+	ui_file = QFile(str(file_path / 'ui/main_layout.ui'))
+	ui_file.open(QIODevice.OpenModeFlag.ReadOnly)
 	
 	loader = QUiLoader()
 	main_window = loader.load(ui_file, None)
