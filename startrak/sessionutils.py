bbe5d3cb5bb8c8a49d685833e7262a74265db7cc
JotaRata <jjbarria.c@gmail.com>
2024-01-19 03:26:04 -0300

    Changed session overwrite action: In the case of load_sesison and new_session, setting overwrite to False will not raise an exception, instead they will just return the new session object

--- startrak/sessionutils.py
+++ startrak/sessionutils.py
@@ -22,13 +22,13 @@ SessionType = Literal['inspect', 'scan']
 __session__ : Session = InspectionSession('default')
 
 @overload
-def new_session(name : str, sessionType : Literal['inspect'], *args, overwrite : bool = False, **kwargs) -> InspectionSession: ...
+def new_session(name : str, sessionType : Literal['inspect'], *args, overwrite : bool, **kwargs) -> InspectionSession: ...
 @overload
-def new_session(name : str, sessionType : Literal['scan'], *args, overwrite : bool = False, **kwargs) -> ScanSession: ...
+def new_session(name : str, sessionType : Literal['scan'], *args, overwrite : bool, **kwargs) -> ScanSession: ...
 
-def new_session(name : str, sessionType : SessionType = 'inspect', *args, overwrite : bool = False, **kwargs) -> Session:
+def new_session(name : str, sessionType : SessionType = 'inspect', *args, overwrite : bool = True, **kwargs) -> Session:
 	'''
-		Creates a new session of the specified type, if another session exists a RuntimeError will be raised unless the overwrite flag is enabled.
+		Creates a new session of the specified type and returns it.
 
 		Parameters:
 		* name (str): The name of the new session
@@ -36,7 +36,7 @@ def new_session(name : str, sessionType : SessionType = 'inspect', *args, overwr
 			* "inspect" will create a emtpy session useful to work with files already saved in disk.
 			* "scan" will actively scan in a given directory if new files were added or removed, useful for real-time analysis. 
 		* *args: Additional arguments for the newly created Session, if sessionType = "scan" then the working directory must be specified here or in **kwargs.
-		* overwrite (bool): Whether the new session should overwrite the previous one if exists, if False then a RunTimeError is raised upon calling this function if another session already exists. Default: False.
+		* overwrite (bool): Whether the new session should overwrite the previous one if exists Default: False.
 		* **kwargs: Additional keywords for the newly created session.
 
 		Returns:
@@ -44,9 +44,6 @@ def new_session(name : str, sessionType : SessionType = 'inspect', *args, overwr
 		* ScanSession if sessionType = "scan"
 	'''
 	global __session__
-	if (__session__ and __session__.name != 'default') and not overwrite:
-		raise RuntimeError(f'A Session of type {type(__session__).__name__} already exists, to overwrite call this function again with overwrite= True')
-
 	session : Session
 	match sessionType:
 		case 'inspect':
@@ -55,8 +52,9 @@ def new_session(name : str, sessionType : SessionType = 'inspect', *args, overwr
 			session = ScanSession(name, *args, **kwargs)
 		case _:
 			raise NameError(f'Invalid session type: "{sessionType}", expected "inspect" or "scan".')	
-
-	__session__ = session
+	
+	if overwrite:
+		__session__ = session
 	return session
 
 def get_session() -> Session:
