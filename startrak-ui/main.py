f7061719a2abee3298f255ff83849a531bed847c
JotaRata <jjbarria.c@gmail.com>
2024-01-20 02:51:46 -0300

    Implemented new changes

--- startrak-ui/main.py
+++ startrak-ui/main.py
@@ -1,12 +1,23 @@
-from PySide6.QtWidgets import QApplication
+import sys, os
+sys.path.insert(0, os.getcwd())
+import startrak
+from PySide6 import QtWidgets, QtCore
 from utils import *
+from classes import SessionTreeModel
 
 if __name__ == "__main__":
-	app = QApplication([])
+	app = QtWidgets.QApplication([])
 	app.setStyle('Fusion')
 
 	with read_layout('main_layout') as f:
 		main = create_widget(f)
+		splitter = get_child(main, 'splitter_subh', QtWidgets.QSplitter)
+		splitter.setSizes([splitter.size().width() // 2, 
+                        splitter.size().width() // 2])
+		session_view = get_child(splitter, 'session_view', QtWidgets.QTreeView, )
+		
+		model = SessionTreeModel()
+		session_view.setModel(model)
 		main.show()
 	app.exec()
 else:
