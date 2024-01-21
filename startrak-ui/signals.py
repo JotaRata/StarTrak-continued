d3a7d7f65d1fefe036ca584a85d213ab7fc75941
JotaRata <jjbarria.c@gmail.com>
2024-01-21 02:13:00 -0300

    Improved signals

--- startrak-ui/signals.py
+++ startrak-ui/signals.py
@@ -1,7 +1,7 @@
 
+from PySide6 import QtCore
 from PySide6.QtWidgets import QFileDialog
 
-
-def open_sessionDialog(st_module, main_window):
+def on_sessionLoad(st_module, main_window):
 	path, _ = QFileDialog.getOpenFileName(main_window, 'Open session', filter= 'Startrak session (*.trak)')
 	st_module.load_session(path)
