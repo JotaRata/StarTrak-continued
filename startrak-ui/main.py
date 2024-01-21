0246dc4efba28799a760f20e8a43759f367b9e76
JotaRata <jjbarria.c@gmail.com>
2024-01-21 02:12:54 -0300

    Removed most logic from main and transferred to their dedicated classes

--- startrak-ui/main.py
+++ startrak-ui/main.py
@@ -1,35 +1,17 @@
 # mypy: disable-error-code="attr-defined"
 import sys, os
 sys.path.insert(0, os.getcwd())
-import startrak
-from PySide6.QtGui import QAction
-from PySide6 import QtWidgets, QtCore
-from utils import *
-from classes import SessionTreeModel
 
-def setup_sessionView(session_view):
-	model = SessionTreeModel(startrak.get_session())
-	session_view.setModel(model)
-	session_view.expand(model.index(0, 0, QtCore.QModelIndex()))
+import startrak
+from qt.extensions import *
+from views import Application, MainView
 
 if __name__ == "__main__":
-	app = QtWidgets.QApplication([])
+	app = Application(startrak, [])
 	app.setStyle('Fusion')
-	with read_layout('main_layout') as f:
-		main = create_widget(f)
-		splitter = get_child(main, 'splitter_subh', QtWidgets.QSplitter)
-		splitter.setSizes([splitter.size().width() // 2, 
-                        splitter.size().width() // 2])
-		session_view = get_child(splitter, 'session_view', QtWidgets.QTreeView, )
-		setup_sessionView(session_view)
 
-		label = get_child(main, 'label_info', QtWidgets.QLabel)
-		session_view.clicked.connect( lambda index:  label.setText(index.internalPointer().ref.__pprint__(0,2)) )
-
-		action_openSession = get_child(main, 'action_open', QAction)
-		action_openSession.triggered.connect(lambda: signals.open_sessionDialog(startrak, main))
-		action_openSession.triggered.connect(lambda: setup_sessionView(session_view))
-		main.show()
+	main_view = MainView()
+	main_view.show()
 	app.exec()
 else:
 	raise ImportError('Cannot import module')
