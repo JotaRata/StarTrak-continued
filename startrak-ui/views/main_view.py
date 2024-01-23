dffbfaceb38f57dd8e48ba1e364fdfae393df1d1
JotaRata <jjbarria.c@gmail.com>
2024-01-23 01:47:51 -0300

    Added new session command to menubar

--- startrak-ui/views/main_view.py
+++ startrak-ui/views/main_view.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
-from PySide6 import QtCore, QtWidgets
+from PySide6 import QtCore, QtWidgets 
 from PySide6.QtGui import QAction
-from PySide6.QtWidgets import QApplication, QFileDialog
+from PySide6.QtWidgets import QFileDialog, QMessageBox
 from qt.extensions import *
 from qt.extensions.qt_loader import STUI_DIR
 from views.application import Application
@@ -29,7 +29,18 @@ class MainView(QtWidgets.QMainWindow, UI_MainWindow):	#type: ignore[valid-type, 
 		app = Application.instance()
 		match code.objectName():
 			case 'action_new':
-				print('New Session')
+				prev_session = app.st_module.get_session()
+				# todo: Check whether the session has unsaved changes
+				safe = not prev_session or prev_session.name == 'default'
+				if not safe:
+					msg = QMessageBox.warning(self, 'Discard session',
+											'A session already exists. Do you want to discard it?',
+											QMessageBox.StandardButton.Cancel | QMessageBox.StandardButton.Discard)
+					safe = msg == QMessageBox.StandardButton.Discard
+				if safe:
+					session = app.st_module.new_session('default')
+					app.on_sessionLoad.emit(session)
+
 			case 'action_open':
 				path, _ = QFileDialog.getOpenFileName(self, 'Open session', filter= 'Startrak session (*.trak)')
 				session = app.st_module.load_session(path)
