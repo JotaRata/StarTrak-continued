95fb47470263a468055b5affa1842840d9b58193
JotaRata <jjbarria.c@gmail.com>
2024-01-23 01:23:54 -0300

    Removed module signals.py

--- startrak-ui/signals.py
+++
@@ -1,7 +0,0 @@
-
-from PySide6 import QtCore
-from PySide6.QtWidgets import QFileDialog
-
-def on_sessionLoad(st_module, main_window):
-	path, _ = QFileDialog.getOpenFileName(main_window, 'Open session', filter= 'Startrak session (*.trak)')
-	st_module.load_session(path)
