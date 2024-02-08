93ff7ae4792a0701b37813bc0a29e8f3c7102833
JotaRata <jjbarria.c@gmail.com>
2024-02-08 16:38:32 -0300

    Added ability to select stars from the view and from the inspector

--- startrak-ui/views/image_view.py
+++ startrak-ui/views/image_view.py
@@ -1,10 +1,13 @@
+from __future__ import annotations
+import enum
 from typing import Callable
+from PySide6.QtWidgets import QGraphicsSceneMouseEvent
 import numpy as np
 from qt.extensions import load_class, get_child
 from qt.classes.range_slider import QRangeSlider
 from PySide6 import QtWidgets, QtCore, QtGui
 from views.application import Application
-from startrak.native import FileInfo, Star
+from startrak.native import FileInfo, Star, StarList
 from startrak.native.alias import ImageLike
 
 UI_ImageViewer, _ = load_class('image_viewer')
@@ -12,7 +15,10 @@ class ImageViewer(QtWidgets.QWidget, UI_ImageViewer):	#type:ignore
 	view : QtWidgets.QGraphicsView
 	level_slider : QRangeSlider
 	current_file : FileInfo | None
+	current_index : QtCore.QModelIndex
 	mapping_func : Callable[[ImageLike], ImageLike]
+	star_labels : list[_StarLabelItem]
+	on_starSelected = QtCore.Signal(QtCore.QModelIndex)
 
 	def __init__(self, parent: QtWidgets.QWidget) -> None:
 		QtWidgets.QWidget.__init__(self, parent)
@@ -24,8 +30,8 @@ class ImageViewer(QtWidgets.QWidget, UI_ImageViewer):	#type:ignore
 		self.level_slider = get_child(self, 'level_slider', QRangeSlider)
 		self.current_file = None
 		self.view.scene().addText('Double click on a file to preview it').setDefaultTextColor(QtCore.Qt.GlobalColor.white)
-		self.pointSize = 1
-
+		self.star_labels = []
+		self.selected_star = -1
 		self.on_levelChange(0, 255)
 		self.on_colormapChange('linear')
 	
@@ -45,8 +51,16 @@ class ImageViewer(QtWidgets.QWidget, UI_ImageViewer):	#type:ignore
 			return
 		if type(pointer.ref) is FileInfo:
 			self.current_file = pointer.ref
+			self.current_index = index
 			array = self.current_file.get_data()
 			self.set_image(array)
+
+		elif type(pointer.ref) is Star:
+			for i, item in enumerate(self.star_labels):
+				item.selected = i == index.row()
+				item.update()
+				if item.selected:
+					self.selected_star = i
 	
 	@QtCore.Slot(str)
 	def on_colormapChange(self, value : str):
@@ -77,39 +91,62 @@ class ImageViewer(QtWidgets.QWidget, UI_ImageViewer):	#type:ignore
 		pixmap_item.setTransformationMode(QtCore.Qt.TransformationMode.SmoothTransformation)
 		scene.setSceneRect(QtCore.QRectF(pixmap.rect()))
 		self.view.fitInView(pixmap_item, QtCore.Qt.AspectRatioMode.KeepAspectRatio)
-		self.pointSize = 1
-		self.draw_stars()
 		setup_itemColors(np.mean(array) > 128)
-	
+		self.draw_stars()
+
 	def draw_stars(self,):
 		session = Application.get_session()
 		stars = session.included_stars
 		scene = self.view.scene()
-		self.view.setMouseTracking(True)
-		for star in stars:
-			item = _StarLabelItem(star)
+		self.star_labels.clear()
+
+		def emit_starSelected(index):
+				model = self.current_index.model()
+				session_idx = model.index(0, 0, QtCore.QModelIndex())
+				parent_idx = model.index(model.rowCount(session_idx) - 1, 0, session_idx)
+				model_index = model.index(index, 0, parent_idx)
+				self.selected_star = index
+				
+				for item in self.star_labels:
+					if item.index == index:
+						continue
+					if item.selected:
+						item.selected = False
+						item.update()
+				self.on_starSelected.emit(model_index)
+
+		for i, star in enumerate(stars):
+			item = _StarLabelItem(i, star)
+			item.selected = self.selected_star == i
+			# todo: Add method to StarList to retrieve indices by name
+			item.on_mouseClick = emit_starSelected
 			scene.addItem(item)
+			self.star_labels.append(item)
+	
 
 class _StarLabelItem(QtWidgets.QGraphicsItem):
-	def __init__(self, star : Star):
+	def __init__(self, index : int, star : Star):
 		super().__init__()
+		self.index = index
 		self.text = star.name
 		self.position = star.position
 		self.radius = star.aperture
 		self.hovered = False
+		self.selected = False
 		self.setFlags(QtWidgets.QGraphicsItem.GraphicsItemFlag.ItemIsFocusable | QtWidgets.QGraphicsItem.GraphicsItemFlag.ItemIsSelectable)
 		self.setAcceptHoverEvents(True)
 		self._bbox = QtCore.QRectF(self.position[0] - self.radius * 2, self.position[1] - self.radius * 2,
 							4 * self.radius, 4 * self.radius)
+		self.on_mouseClick = None
 
 	def boundingRect(self):
 		return self._bbox
 
 	def paint(self, painter, option, widget):
 		scale = 1.0 / painter.transform().m22()
-		color = hover_color if self.hovered else normal_color
-		alpha = 1 if self.hovered else 0.5 if self.radius >= 8 else 0
-		pad = 2 if self.hovered else 0
+		color = highlighted_color if self.selected  else hover_color if self.hovered else normal_color
+		alpha = 1 if self.hovered or self.selected else 0.5 if self.radius >= 8 else 0
+		pad = 2 if self.hovered or self.selected else 0
 
 		painter.setPen(QtGui.QPen(color, scale + 2))
 		crect = QtCore.QRectF(self.position[0] - self.radius, self.position[1] - self.radius,
@@ -124,25 +161,30 @@ class _StarLabelItem(QtWidgets.QGraphicsItem):
 		painter.drawText(rect, QtCore.Qt.AlignCenter, self.text)
 		self._bbox = rect.united(crect)
 
+	def mouseDoubleClickEvent(self, event: QGraphicsSceneMouseEvent) -> None:
+		super().mouseDoubleClickEvent(event)
+		self.selected = True
+		if self.on_mouseClick:
+			self.on_mouseClick(self.index)
+		self.update()
+	
 	def hoverEnterEvent(self, event):
 		super().hoverEnterEvent(event)
-		print("Mouse entered", self.text)
 		self.hovered = True
 		self.update()
 
 	def hoverLeaveEvent(self, event):
 		super().hoverLeaveEvent(event)
-		print("Mouse left", self.text)
 		self.hovered = False
 		self.update()
 
 def setup_itemColors(inverted):
-	global normal_color, hover_color, selected_color
+	global normal_color, hover_color, selected_color, highlighted_color
+	normal_color = Application.instance().styleSheet().get_color('secondary')
+	highlighted_color = Application.instance().styleSheet().get_color('highlighted')
 	if inverted:
-		normal_color = Application.instance().styleSheet().get_color('secondary')
 		hover_color = Application.instance().styleSheet().get_color('secondary-dark')
 		selected_color = Application.instance().styleSheet().get_color('secondary-dark')
 	else:
-		normal_color = Application.instance().styleSheet().get_color('secondary')
 		hover_color = Application.instance().styleSheet().get_color('secondary-light')
 		selected_color = Application.instance().styleSheet().get_color('secondary-light')
