ab9fb53f9bcd556b794449cbf66347df34df5ca4
JotaRata <36273501+JotaRata@users.noreply.github.com>
2024-02-29 15:12:29 -0300

    Imrpoved console functionality

--- startrak-ui/views/console_view.py
+++ startrak-ui/views/console_view.py
@@ -1,28 +1,29 @@
 from io import BytesIO, TextIOBase
+import os
 import sys
 
-from PySide6.QtCore import Qt, Signal
+from PySide6.QtCore import QObject, Qt, Signal, Slot
 from PySide6.QtGui import QFont, QTextBlockFormat
 from qt.extensions import *
 from PySide6 import QtWidgets
 
 import startrak
 
-class ConsoleView(QtWidgets.QFrame):
+UI_CONSOLE, _ = load_class('console_view')
+class ConsoleView(QtWidgets.QFrame, UI_CONSOLE):	#type:ignore
+	mode : int
 	def __init__(self, parent: QtWidgets.QWidget = None):
 		super().__init__(parent)
-		layout = QtWidgets.QVBoxLayout(self)
-		self.scroll_text = QtWidgets.QTextEdit(self)
-		self.scroll_text.setReadOnly(True)
-		font = QFont("Courier", 10)
-		self.scroll_text.setFont(font)
+		self.setupUi(self)
+
+		self.scroll_text = get_child(self, 'scroll_text', QtWidgets.QTextEdit)
+		self.line_input = get_child(self, 'line_input', QtWidgets.QLineEdit)
+		self.mode_selector = get_child(self, 'mode_selector', QtWidgets.QComboBox)
+
+		# todo: replace with actual output from Startrak CL
 		self.scroll_text.textCursor().clearSelection()
 		self.scroll_text.insertPlainText('Startrak ' + startrak.VERSION + '\n')	#type:ignore
 		self.scroll_text.insertPlainText('=' * 40 + '\n')
-
-		self.input_line = QtWidgets.QLineEdit(self)
-		layout.addWidget(self.scroll_text)
-		layout.addWidget(self.input_line)
 
 		self.stdout = StdoutListener()
 		self.stderr = StderrListener()
@@ -31,34 +32,62 @@ class ConsoleView(QtWidgets.QFrame):
 		self.stdout.write_event.connect(self.update_console)
 		self.stderr.write_event.connect(self.update_console)
 
+		self.mode = 0
+		self.mode_selector.addItem('>', 'Python')
+		self.mode_selector.addItem('!', 'Bash/CMD')
+		self.mode_selector.setItemDelegate(SelectorBoxDelegate())
+
 		block_format = QTextBlockFormat()
 		block_format.setLineHeight(1.5, 0x4)
 		self.scroll_text.textCursor().setBlockFormat(block_format)
-
 		self._globals = {val: getattr(startrak, val) for val in dir(startrak) if '__' not in val}
-		self.input_line.returnPressed.connect(self.command_sent)
 
 	def update_console(self, text : str):
 		self.scroll_text.moveCursor(QtGui.QTextCursor.MoveOperation.End)
 		self.scroll_text.insertPlainText(text)
 		self.scroll_text.moveCursor(QtGui.QTextCursor.MoveOperation.End)
 
+	@Slot(int)
+	def set_mode(self, mode : int):
+		self.mode = mode
+
+	@Slot(str)
+	def text_edited(self, text : str):
+		lstrip = text.lstrip()
+		if lstrip.startswith('>'):
+			self.mode_selector.setCurrentIndex(0)
+			self.line_input.setText(lstrip[1:])
+		if lstrip.startswith('!'):
+			self.mode_selector.setCurrentIndex(1)
+			self.line_input.setText(lstrip[1:])
+
+	@Slot()
 	def command_sent(self):
-		text = self.input_line.text()
-		if not text:
-			return
-		self.update_console('> ' + text + '\n')
-
-		try:
-			result = eval(text, self._globals)
-		except SyntaxError:
-			exec(text, self._globals)
-			result = None
-		finally:
-			self.input_line.clear()
-
-		if result:
-			self.update_console(repr(result) + '\n')
+		text = self.line_input.text()
+		if self.mode == 0:
+			if not text:
+				self.update_console('> ' + '\n')
+				return
+			self.update_console('> ' + text + '\n')
+			try:
+				result = eval(text, self._globals)
+			except SyntaxError:
+				exec(text, self._globals)
+				result = None
+			finally:
+				self.line_input.clear()
+			if result:
+				self.update_console(repr(result) + '\n')
+		
+		elif self.mode == 1:
+			if not text:
+				return
+			self.update_console('! ' + text + '\n')
+			result = os.popen(text).read()
+			if result:
+				self.update_console(result + '\n')
+			self.line_input.clear()
+			self.mode_selector.setCurrentIndex(0)
 
 class StdoutListener(QtCore.QObject):
 	write_event = Signal(str)
@@ -85,3 +114,15 @@ class StderrListener(QtCore.QObject):
 			self.write_event.emit(s + '\n')
 	def flush(self):
 		self.__stderr__.flush()
+
+class SelectorBoxDelegate(QtWidgets.QStyledItemDelegate):
+	def paint(self, painter, option, index):
+		text = index.data(Qt.ItemDataRole.DisplayRole)
+		symbol = index.data(Qt.ItemDataRole.UserRole)
+		
+		comboBoxStyle = QtWidgets.QStyleOptionComboBox()
+		comboBoxStyle.initFrom(option.widget)
+		comboBoxStyle.currentText = symbol
+		comboBoxStyle.rect = option.rect
+		comboBoxStyle.state |= QtWidgets.QStyle.State_Enabled
+		QtWidgets.QApplication.style().drawControl(QtWidgets.QStyle.CE_ComboBoxLabel, comboBoxStyle, painter)
