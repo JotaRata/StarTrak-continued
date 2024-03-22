bb99f20e482abf6e6329df51e93dca7a1716af63
JotaRata <36273501+JotaRata@users.noreply.github.com>
2024-03-21 23:56:09 -0300

    Cleanup

--- startrak-cl/__main__.py
+++ startrak-cl/__main__.py
@@ -1,12 +1,9 @@
 import os
 import sys
-
 # os.environ['ST_SESSION_DISABLED'] = '1'
 sys.path.append(os.getcwd())
-
-import startrak
-from startrak.native import Star
-
-startrak.add_star(Star('test', (0,0)))
 from _app.shell import ShellConsole
+import startrak
+
+
 app = ShellConsole()
