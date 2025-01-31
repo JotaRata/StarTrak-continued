0a32a56ce50b5524daa3da96daee3c030beec2bf
JotaRata <36273501+JotaRata@users.noreply.github.com>
2025-01-30 20:04:49 -0400

    Changed command class to use metaclasses instead

--- startrak_cl/commands.py
+++ startrak_cl/commands.py
@@ -1,61 +1,51 @@
 from __future__ import annotations
-import inspect
-import os
-import sys
 from typing import Any, Callable, Self
 from packaging.version import Version
 
+__all__ = ['Command', 'Parameter', 'Optional']
 
-class CommandBuilder:
-	def __init__(self, name : str, version : str = "1.0.0"):
-		self._name = name
-		self._version = version
-		self._imports = list[str]()
-		self._params = list[Parameter]()
+class _AbstractCommandMeta(type):
+	registered_commands : dict[str, type] = {}
 
-		self._file_name : str = None
-		self._block_start = None
-		self._block_end = None
+	def __new__(cls, class_name : str, bases : tuple[type,...], namespace : dict[str, Any], *,
+					alias : str = None, 
+					description : str = None, 
+					author : str = None, 
+					version : str = '1.0.0'):
+		
+		klass : type = super().__new__(cls, class_name, bases, namespace) 
+		if class_name == 'Command':
+			return klass
+		
+		assert alias, 'Command requires one parameter "alias"'
+		assert description, 'Command requires one parameter "description"'
+		assert 'init_params' in namespace, 'Command requires defining an "init_params" method'
+		assert 'execute' in namespace, 'Command requires defining an "execute" method'
 
-	def add_author(self, author : str) -> Self:
-		assert type(author) is str
-		self._author = author
-		return self
-	
-	def add_description(self, description : str) -> Self:
-		assert type(description) is str
-		self._desc = description
-		return self
+		assert '__init__' not in namespace, 'Cannot override "__init__" method from Command.'
+		assert '__init_subclass__' not in namespace, 'Cannot override "__init_subclass__" method from Command.'
 
-	def add_manual(self, manual_file : str) -> Self:
-		assert type(manual_file) is str
-		assert os.path.isfile(manual_file)
-		self._man = manual_file
-		return self
-	
-	def add_import(self, package_name : str) -> Self:
-		assert type(package_name) is str
-		self._imports.append(package_name)
-		return self
-	
-	def add_parameter(self, parameter : Parameter) -> Self:
-		assert isinstance(parameter, Parameter)
-		self._params.append(parameter)
-		return self
-	
-	def __enter__(self):
-		return self
-	
-	def __exit__(self, *args):
-		pass
+		klass.__alias__ = alias
+		klass.__desc__ = description
+		klass.__author__ = author
+		klass.__version__ = Version(version)
 
-	def get_context(self) -> CommandExecutor:
-		return CommandExecutor(self)
+		_AbstractCommandMeta.registered_commands[klass.__alias__] = klass
+		return klass
 	
-	# def executes(self, function : Callable[..., Output]) -> Self:
-	# 	assert callable(function)
-	# 	self._function = function
-	# 	return self
+class Command(metaclass= _AbstractCommandMeta):
+	def __init__(self):
+		raise TypeError(f'Cannot instantiate object of type "{type(self).__name__}"')
+	
+	def __init_subclass__(cls):
+		if cls.__base__ != Command:
+			raise TypeError(f'Cannot derive from class "{cls.__base__.__name__}"')
+	
+	def init_params() -> list[Parameter]:
+		raise NotImplementedError()
+
+	def execute(*args : tuple[Parameter, ...]) -> None:
+		raise NotImplementedError()
 
 
 class Parameter:
@@ -88,52 +78,3 @@ class Optional(Parameter):
 		self._default = default
 		return self
 	
-
-class Output:
-	def returns_value(self, value : object) -> Self:
-		self._value = value
-		return self
-	
-	def returns_text(self, source : Callable[..., str], **kwargs) -> Self:
-		assert callable(source)
-		self._text_source = source, kwargs
-		return self
-	
-class CommandExecutor:
-	def __init__(self, builder : CommandBuilder):
-		self._builder = builder
-		
-	def __enter__(self):
-		sys.settrace(lambda *args, **keys: None)
-		frame = sys._getframe(1)
-		frame.f_trace = self.trace
-
-		return CommandExecutor.CommandHelper()
-	
-	def trace(self, frame, event, arg):
-		raise CommandExecutor.SkipBlockExecution('Make sure to encapsulate the code in try/except')
-
-	def __exit__(self, type, value, traceback):
-		frame = inspect.currentframe()
-		try:
-			info = inspect.getframeinfo(frame.f_back)
-			self._builder._file_name = info.filename
-			self._builder._block_start = info.positions.lineno
-			self._builder._block_end = info.positions.end_lineno
-		finally:
-			del frame
-		if type is CommandExecutor.SkipBlockExecution:
-			return True
-		return False
-
-	class CommandHelper:
-		def print(self, value : str):
-			print(value)
-		def read(self):
-			pass
-	
-	class SkipBlockExecution(Exception):
-		pass
-
-def register_command(command : CommandBuilder) -> CommandBuilder:
-	return command
