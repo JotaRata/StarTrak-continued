2351067d5c4a022ff2c625ed0a7a1d6ccc8974c5
JotaRata <jjbarria.c@gmail.com>
2025-03-28 01:22:02 -0300

    Added file operation commands

--- startrak_cl/source/base_commands.py
+++ startrak_cl/source/base_commands.py
@@ -2,7 +2,7 @@
 import os
 import sys
 from startrak_cl import STException
-from startrak_cl.commands import Command, Parameter, get_active_console
+from startrak_cl.commands import Command, Optional, Parameter, get_active_console
 from startrak_cl.utils.casters import path
 
 class ChangeDirectoryCommand(Command,
@@ -75,3 +75,84 @@ class QuitCommand(Command,
 		
 		def execute(*args, **kwargs):
 			quit()
+
+class CopyCommand(Command,
+					alias= 'cp',
+					description= 'Copies a file to another location.',
+					author= 'JotaRata - Adapted from GNU'):
+	def init_params():
+		return [
+			Parameter('source')
+				.with_description('The source file to copy.')
+				.with_type(str),
+			
+			Parameter('destination')
+				.with_description('The destination file to copy to.')
+				.with_type(str)
+		]
+	
+	def execute(source : str, destination : str, **kwargs):
+		if not os.path.isfile(source):
+			raise STException('Source is not a file.')
+		
+		if os.path.isdir(destination):
+			destination = os.path.join(destination, os.path.basename(source))
+		if os.path.exists(destination):
+			raise STException('Destination already exists.')
+		
+		with open(source, 'rb') as src:
+			with open(destination, 'wb') as dest:
+				dest.write(src.read())
+
+class MoveCommand(Command, 
+					alias= 'mv',
+					description= 'Moves a file to another location.',
+					author= 'JotaRata - Adapted from GNU'):
+	def init_params():
+		return [
+			Parameter('source')
+				.with_description('The source file to move.')
+				.with_type(str),
+			
+			Parameter('destination')
+				.with_description('The destination file to move to.')
+				.with_type(str)
+		]
+	
+	def execute(source : str, destination : str, **kwargs):
+		if not os.path.isfile(source):
+			raise STException('Source is not a file.')
+		
+		if os.path.isdir(destination):
+			destination = os.path.join(destination, os.path.basename(source))
+		if os.path.exists(destination):
+			raise STException('Destination already exists.')
+		
+		os.rename(source, destination)
+
+class RemoveCommand(Command,
+					alias= 'rm',
+					description= 'Removes a file or an empty directory.',
+					author= 'JotaRata - Adapted from GNU'):
+	def init_params():
+		return [
+			Parameter('target')
+				.with_description('The file or directory to remove.')
+				.with_type(str),
+			Optional('recursive', 'r')
+				.with_description('Recursively remove directories and their contents.')
+				.with_type(bool),
+		]
+	
+	def execute(target : str, recursive : bool = False, **kwargs):
+		if not os.path.exists(target):
+			raise STException('Target does not exist.')
+		
+		if os.path.isdir(target):
+			if recursive:
+				os.rmdir(target)
+			else:
+				raise STException('Target is a directory. Use -r to remove it recursively.')
+		else:
+			os.remove(target)
+	
